c33c9e4f810c525bedd3fcfd1b0c59cf4cb5027c
Borislav <91478846+L1sa620@users.noreply.github.com>
2026-03-01 12:38:02 +0300

    AISD 2 practice

--- main.cpp
+++ main.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
+template<class T>
+T* merge(const T* a, size_t sa,
+         const T* b, size_t sb,
+         T* c)
+{
+  size_t ia = 0;
+  size_t ib = 0;
+  size_t ic = 0;
+  while (ia < sa && ib < sb)
+  {
+    if (a[ia] <= b[ib])
+      {
+        c[ic++] = a[ia++];
+      }
+    else
+    {
+      c[ic++] = b[ib++];
+    }
+  }
+  while (ia < sa)
+  {
+    c[ic++] = a[ia++];
+  }
+  while (ib < sb)
+  {
+    c[ic++] = b[ib++];
+  }
+  return c;
+}
 
 int main()
 {
